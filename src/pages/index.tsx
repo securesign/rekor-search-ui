5f0b598bf065bc99451b74675e4f549ae00436b0
kahboom <rachelyordan@gmail.com>
2024-02-14 14:45:49 +0000

    feat(layout): add tas logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import {
-	Container
-} from "@mui/material";
+import { Container } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
@@ -9,7 +7,11 @@ import {
 	Masthead,
 	MastheadContent,
 	MastheadMain,
-	Page, Toolbar, ToolbarContent, ToolbarGroup, ToolbarItem
+	Page,
+	Toolbar,
+	ToolbarContent,
+	ToolbarGroup,
+	ToolbarItem,
 } from "@patternfly/react-core";
 import { RekorClientProvider } from "../modules/api/context";
 import { Explorer } from "../modules/components/Explorer";
@@ -22,61 +24,71 @@ const Home: NextPage = () => {
 	const [settingsOpen, setSettingsOpen] = useState(false);
 
 	return (
-		<Page header={
-			<Masthead>
-				<MastheadMain>
-					<Link href={"/"} className={"pf-v5-c-masthead_brand"}>
-						<Image
-							className={"pf-v5-c-brand"}
-							src={"/Logo-Red_Hat-Trusted_Artifact_Signer-A-Reverse-RGB.svg"}
-							alt={"Red Hat Trusted Artifact Signer logo"}
-							priority={true}
-							width={198}
-							height={42}
-						/>
-					</Link>
-				</MastheadMain>
-				<MastheadContent>
-					<Toolbar
-						ouiaId="masthead-toolbar"
-						id={"masthead-toolbar"}
-						isFullHeight
-						isStatic
-					>
-						<ToolbarContent id={"masthead-toolbar"}>
-							<ToolbarGroup
-								variant="icon-button-group"
-								align={{ default: "alignRight" }}
-								spacer={{ default: "spacerNone", md: "spacerMd" }}
-							>
+		<Page
+			header={
+				<Masthead>
+					<MastheadMain>
+						<Link
+							href={"/"}
+							className={"pf-v5-c-masthead_brand"}
+						>
+							<Image
+								className={"pf-v5-c-brand"}
+								src={"/Logo-Red_Hat-Trusted_Artifact_Signer-A-Reverse-RGB.svg"}
+								alt={"Red Hat Trusted Artifact Signer logo"}
+								priority={true}
+								style={{ height: 48 }}
+								width={200}
+								height={48}
+							/>
+						</Link>
+					</MastheadMain>
+					<MastheadContent>
+						<Toolbar
+							ouiaId="masthead-toolbar"
+							id={"masthead-toolbar"}
+							isFullHeight
+							isStatic
+						>
+							<ToolbarContent id={"masthead-toolbar"}>
 								<ToolbarGroup
 									variant="icon-button-group"
-									visibility={{ default: "hidden", lg: "visible" }}
+									align={{ default: "alignRight" }}
+									spacer={{ default: "spacerNone", md: "spacerMd" }}
 								>
-									<ToolbarItem>
-										<Button
-											aria-label="Settings"
-											onClick={() => setSettingsOpen(true)}
-											variant={"plain"}
-											icon={<CogIcon />}
-											ouiaId={"setting-button"}
-										/>
-									</ToolbarItem>
-									<ToolbarItem>
-										<Button component="a" href="https://github.com/sigstore/rekor-search-ui" icon={<GithubIcon />}
-														target="_blank"
-														variant={"plain"}
-														ouiaId={"github-link"}
-														aria-label={"GitHub Link"}
-														rel="noopener noreferrer" />
-									</ToolbarItem>
+									<ToolbarGroup
+										variant="icon-button-group"
+										visibility={{ default: "hidden", lg: "visible" }}
+									>
+										<ToolbarItem>
+											<Button
+												aria-label="Settings"
+												onClick={() => setSettingsOpen(true)}
+												variant={"plain"}
+												icon={<CogIcon />}
+												ouiaId={"setting-button"}
+											/>
+										</ToolbarItem>
+										<ToolbarItem>
+											<Button
+												component="a"
+												href="https://github.com/sigstore/rekor-search-ui"
+												icon={<GithubIcon />}
+												target="_blank"
+												variant={"plain"}
+												ouiaId={"github-link"}
+												aria-label={"GitHub Link"}
+												rel="noopener noreferrer"
+											/>
+										</ToolbarItem>
+									</ToolbarGroup>
 								</ToolbarGroup>
-							</ToolbarGroup>
-						</ToolbarContent>
-					</Toolbar>
-				</MastheadContent>
-			</Masthead>
-		}>
+							</ToolbarContent>
+						</Toolbar>
+					</MastheadContent>
+				</Masthead>
+			}
+		>
 			<Head>
 				<title>Rekor Search</title>
 				<meta
@@ -99,7 +111,7 @@ const Home: NextPage = () => {
 					mt: 4,
 					display: "flex",
 					flexDirection: "column",
-					gap: 3
+					gap: 3,
 				}}
 			>
 				<Explorer />
