6f2eeba4e5dfdd59d33eba0d615d41b2737e4d4a
kahboom <rachelyordan@gmail.com>
2024-03-04 16:45:29 +0000

    fix: transpile patternfly icons package
    fix: typo in start.sh script
    
    fix: update rekor server endpoint
    
    fix: make value dynamic
    
    fix: use runtimeConfig to resolve endpoint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,30 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+const nextPublicENV = Object.keys(process.env)
+	.filter(key => key.startsWith("NEXT_PUBLIC_"))
+	.reduce(
+		(env, key) => {
+			env[key] = process.env[key] ?? "";
+			return env;
+		},
+		{} as { [key: string]: string },
+	);
+
 class AppDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
 				<Head></Head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+            console.table(${JSON.stringify(nextPublicENV)});
+		        window.process = window.process || {};
+		        window.process.env = window.process.env || {};
+			      Object.assign(window.process.env, ${JSON.stringify(nextPublicENV)});
+            `,
+					}}
+				></script>
 				<body>
 					<Main />
 					<NextScript />
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN=$(http://rekor-server.rekor-server.svc.cluster.local)
-
-# Export the NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN as an environment variable
-export NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN
-
-# Run npm start
-exec npm start
