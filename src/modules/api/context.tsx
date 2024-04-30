8da94d664d59605c406c8f6891eb1de8359781bb
kahboom <rachelyordan@gmail.com>
2024-04-30 16:18:54 +0100

    fix(build): use local baseUrl
    fix(build): use local baseUrl
    
    lint fix

--- src/modules/api/context.tsx
+++ src/modules/api/context.tsx
@@ -7,7 +7,6 @@ import {
 	useState,
 } from "react";
 import { RekorClient } from "rekor";
-import getConfig from "next/config";
 
 export interface RekorClientContext {
 	client: RekorClient;
@@ -23,21 +22,10 @@ export const RekorClientProvider: FunctionComponent<PropsWithChildren<{}>> = ({
 	children,
 }) => {
 	const [baseUrl, setBaseUrl] = useState<string>();
-	const { publicRuntimeConfig } = getConfig();
 
 	const context: RekorClientContext = useMemo(() => {
-		/*
-		Using the Next.js framework, the NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN env variable requires
-		a NEXT_PUBLIC_* prefix to make the value of the variable accessible to the browser.
-		Variables missing this prefix are only accessible in the Node.js environment.
-		https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables
-		*/
-		if (baseUrl === undefined) {
-			if (publicRuntimeConfig.NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN) {
-				setBaseUrl(publicRuntimeConfig.NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN);
-			} else {
-				setBaseUrl("https://rekor.sigstore.dev");
-			}
+		if (baseUrl === undefined && process.env.NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN) {
+			setBaseUrl(process.env.NEXT_PUBLIC_REKOR_DEFAULT_DOMAIN);
 		}
 
 		return {
