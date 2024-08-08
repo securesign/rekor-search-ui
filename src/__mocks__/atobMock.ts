fc6bea21ec70e8ae5cdbbeeebec04eabb8e066c0
kahboom <rachelyordan@gmail.com>
2024-08-08 13:25:40 +0100

    fix: formatting

--- src/__mocks__/atobMock.ts
+++ src/__mocks__/atobMock.ts
@@ -10,12 +10,6 @@ const atobMock = () => {
 
 		return Buffer.from(str, "base64").toString("utf-8");
 	});
-
-	// window.atob = jest.fn().mockImplementation(str => {
-	// 	// console.log(`Decoding: ${str}, Result: ${Buffer.from(str, "base64").toString("utf-8")}`);
-	//
-	// 	return Buffer.from(str, "base64").toString("utf-8");
-	// });
 };
 
 export default atobMock;
